768e9831565be0c7627bdbb5ebe46aa12cbc9d6f
Pool Malca <poolmalca09@gmail.com>
2021-06-19 01:34:14 -0500

    25. Cliente de Cassandra Go

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # bookstore-outh-api
+
+## Creacion Cassandra Table
+
+- Crear Keyspace
+````
+CREATE KEYSPACE IF NOT EXISTS oauth WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};
+````
+
+- Usar Keyspace
+````
+use oauth;
+````
+
+- Crear Tabla
+````
+create table access_token
+(
+    access_token varchar primary key,
+    user_id bigint,
+    client_id bigint,
+    expires bigint
+);
+````
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/burnout09/bookstore-outh-api/src/clients/cassandra"
 	"github.com/burnout09/bookstore-outh-api/src/domain/access_token"
 	"github.com/burnout09/bookstore-outh-api/src/utils/errors"
 )
@@ -16,5 +17,11 @@ func NewRepository() DbRepository {
 }
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	
 	return nil, errors.NewInternalServerError("database connection not implemented yet!")
 }
